e5ffae94c37a48f91ea47495f0cfbda0d006382c
saurabh <saurabhp.bfc@gmail.com>
2021-01-28 11:52:05 +0530

    singlefiles1

--- backend/server.js
+++ backend/server.js
@@ -29,7 +29,7 @@ import bodyParser from 'body-parser';
 //import orderRouter from './routes/orderRouter.js';
 
 import morgan from 'morgan';
-import db from './config.js';
+import config from './config.js';
 import cors from 'cors';
 import Axios from 'axios';
 var Schema = mongoose.Schema;

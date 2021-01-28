2d699bc9cf63e9f68c94357683e3321604b8e431
saurabh <saurabhp.bfc@gmail.com>
2021-01-28 12:04:06 +0530

    singlefiles1

--- backend/config.js
+++ backend/config.js
@@ -22,25 +22,7 @@ module.exports =db; */
 
 // reactcrud is database name
 // 192.168.1.71:27017 is your mongo server name
-import mongoose from 'mongoose';
 export default {
-	const options = {
-		  useMongoClient: true,
-		  autoIndex: false, // Don't build indexes
-		  reconnectTries: Number.MAX_VALUE, // Never stop trying to reconnect
-		  reconnectInterval: 500, // Reconnect every 500ms
-		  poolSize: 10, // Maintain up to 10 socket connections
-		  // If not connected, return errors immediately rather than waiting for reconnect
-		  bufferMaxEntries: 0
-		};
-	mongoose.Promise = global.Promise;
-	var db = 
-	mongoose.connect("mongodb://localhost:27017/wms", options , function(err, response){
-	   if(err){ console.log('Failed to connect to ' + db); }
-	   //else{ console.log('Connected to ' + db, ' + ', response); }
-	   else{ console.log('Connected to ' + db); }
-	});
-
-
-	//module.exports =db;
+    MONGODB_URL: process.env.MONGODB_URL || 'mongodb://localhost:27017/wms',
+    JWT_SECRET: process.env.JWT_SECRET || 'somethingsecret'
 }
